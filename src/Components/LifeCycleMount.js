3d87e5f089435c3c14b06044508f45bd757d2371
Chulbul.JI <chulbul.ji@cognizant.com>
2023-12-21 12:17:46 +0000

    first commit

--- src/Components/LifeCycleMount.js
+++ src/Components/LifeCycleMount.js
@@ -35,10 +35,36 @@ class LifeCycleMount extends React.Component {
     };
 
     // Define componentDidMount method to change state value with `this.getContact(0)` after 3 seconds (3000 ms)
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                name: this.getContact(0).name,
+                contact: this.getContact(0).contact
+            });
+        }, 3000);
+    }
+    
     
     // Define componentDidUpdate method to change state value with `this.getContact(1)` after 3 seconds (3000 ms)
+    componentDidUpdate() {
+        setTimeout(() => {
+            this.setState({
+                name: this.getContact(1).name,
+                contact: this.getContact(1).contact
+            });
+        }, 3000);
+    }
 
     // Define componentWillUnmount method to change state value with `this.getContact(2)` after 3 seconds (3000 ms)
 
+    componentWillUnmount() {
+        setTimeout(() => {
+            this.setState({
+                name: this.getContact(2).name,
+                contact: this.getContact(2).contact
+            });
+        }, 3000);
+    }
 }
 export default LifeCycleMount;
